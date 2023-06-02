359d5ed1591d6cdf83f2ed7ca1e74420a4b2789b
Jacob <jacob@f-lab.kr>
2023-06-01 19:33:56 -0700

    Add log

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, current_app
 from pandas import read_csv
 
 df = read_csv("./data/online_retail.csv")
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='data', static_url_path='')
 
 @app.route("/<int:year>/<int:month>/<int:day>", methods=['GET'])
 def view(year, month, day):
   res = df[df['InvoiceDate'].str.contains(f'{month}/{day}/{year}', case=False, na=False)]
   return res.to_json(orient='records')
+
